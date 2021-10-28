6cc77c0f702a655b5cdbf4b6ac29b83cdc36e453
Anton Melnyk <anton.melnyk@printify.com>
2021-10-28 12:10:34 +0300

    Implement reading from memory

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,9 @@
 use std::ffi::CString;
 use std::path::Path;
 
-use crate::bindings::*;
+use crate::bindings::{xsltParseStylesheetFile, xsltParseStylesheetDoc};
+use crate::libxml::bindings::xmlReadMemory;
+
 use crate::stylesheet::Stylesheet;
 
 /// Load an XSLT stylesheet from (typically `.xsl`) file.
@@ -23,4 +25,33 @@ pub fn parse_file(path_str: &str) -> Result<Stylesheet, String> {
       }
     }
   }
+}
+
+/// Load an XSLT stylesheet from string UTF-8 in byte format
+pub fn parse_string(file_string: Vec<u8>, url: &str) -> Result<Stylesheet, String> {
+    unsafe {
+      let xsl_file_string_len = file_string.len() as i32;
+      let xsl_file_c_str = CString::new(file_string).unwrap();
+      let url_c_str = CString::new(url).unwrap();
+
+      let bytes = xsl_file_c_str.as_bytes_with_nul();
+      let ptr = bytes.as_ptr();
+      let file = ptr as *const i8;
+
+      let bytes = url_c_str.as_bytes_with_nul();
+      let ptr = bytes.as_ptr();
+      let url = ptr as *const i8;
+
+      let xml = xmlReadMemory(
+        file,
+        xsl_file_string_len,
+        url, 
+        0 as *const i8,
+        0
+      );
+
+      let ptr = xsltParseStylesheetDoc(xml);
+
+      Ok(Stylesheet { ptr })
+    }
 }
--- tests/base_tests.rs
+++ tests/base_tests.rs
@@ -3,6 +3,8 @@
 extern crate libxml;
 extern crate libxslt;
 
+use std::fs;
+
 use libxml::tree::Document;
 use libxml::{parser::Parser as XMLParser, tree::SaveOptions};
 use libxslt::parser as xslt_parser;
@@ -17,16 +19,30 @@ fn hello_builder() {
     let source_result = xml_parser.parse_file("tests/data/1.xml");
     assert!(source_result.is_ok());
     let source = source_result.unwrap();
+
+    // Parse XSL file from the file system
     let stylesheet_result = xslt_parser::parse_file("tests/data/1.xsl");
     assert!(stylesheet_result.is_ok());
     let mut stylesheet = stylesheet_result.unwrap();
+
+    // Parse XSL file from memory
+    let stylesheet_string_bytes = fs::read("tests/data/1.xsl").unwrap();
+    let stylesheet_from_string_result = xslt_parser::parse_string(stylesheet_string_bytes, "1");
+    assert!(stylesheet_from_string_result.is_ok());
+    let mut stylesheet_from_string = stylesheet_from_string_result.unwrap();
 
     let source_len = source.to_string().len();
     assert!(source_len > 1000);
 
     let new_doc_result = stylesheet.transform(&source);
+    let new_doc_from_string_result = stylesheet_from_string.transform(&source);
     assert!(new_doc_result.is_ok());
+    assert!(new_doc_from_string_result.is_ok());
     let new_doc: Document = new_doc_result.unwrap();
+    let new_doc_from_string: Document = new_doc_from_string_result.unwrap();
+
+    assert_eq!(new_doc.to_string(), new_doc_from_string.to_string());
+
     let new_serialized = new_doc.to_string_with_options(SaveOptions {
         format: true,
 
